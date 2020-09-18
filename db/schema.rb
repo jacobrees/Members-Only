779d25bf0a8e43fa436aa028c50697e409555a33
rahalrazika <rahalraika@gmail.com>
2020-09-18 12:38:14 +0200

    fix the db:migrate error

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_092721) do
-
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 2020_09_18_093949) do
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "post"
     t.integer "user_id"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
